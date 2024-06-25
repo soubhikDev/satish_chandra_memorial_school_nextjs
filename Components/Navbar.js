9920633f2174c0850a21a065852812288bc14322
Soubhik <soubhikdebnath008@gmail.com>
2024-06-25 16:42:08 +0530

    nav Section

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,6 +1,10 @@
-import React from 'react'
+
+import Link from 'next/link'
+import React, { useState } from 'react'
 
 export default function Navbar() {
+        const [drop , setDrop] = useState(0)
+        console.log(drop);
   return (
     <>
         <div className='Navbar_main'>
@@ -11,18 +15,26 @@ export default function Navbar() {
             </div>
             {/* =============== Navbar Menu =============== */}
             <div className='Navbar_Menu'>
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Academics</li>
-                    <li>Administration</li>
-                    <li>Admission</li>
-                    <li>Co-Curriculum</li>
-                    <li>Facilities</li>
-                    <li>Growth Activities</li>
-                    <li>Support & Libraries</li>
-                    <li>Media Gallery</li>
-                    <li>Contact Us</li>
+                <ul className='Navbar_UL_main'>
+                    <li className='Navbar_LI_main'>Home</li>
+                    <li className='Navbar_LI_main' onClick={() => setDrop(1)} >
+                        <span>About Us</span> 
+                        <ul className={drop == 1 ? 'Navbar_UL_SUB' : "Navbar_UL_SUB2"} onMouseLeave={() => setDrop(0)}>
+                            <li>History</li>
+                            <li>Infrastructure</li>
+                            <li>Vision & Mission</li>
+                            <li>School Calendar</li>
+                        </ul>
+                    </li>
+                    <li className='Navbar_LI_main'>Academics</li>
+                    <li className='Navbar_LI_main'>Administration</li>
+                    <li className='Navbar_LI_main'>Admission</li>
+                    <li className='Navbar_LI_main'>Co-Curriculum</li>
+                    <li className='Navbar_LI_main'>Facilities</li>
+                    <li className='Navbar_LI_main'>Growth Activities</li>
+                    <li className='Navbar_LI_main'>Support & Libraries</li>
+                    <li className='Navbar_LI_main'>Media Gallery</li>
+                    <li className='Navbar_LI_main'>Contact Us</li>
                 </ul>
             </div>
         </div>
