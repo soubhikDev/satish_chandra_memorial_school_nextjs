3136220c18692ff5e30967bf46a3dd70c5395747
Soubhik <soubhikdebnath008@gmail.com>
2024-06-26 15:41:40 +0530

    nav Section

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -57,12 +57,65 @@ export default function Navbar() {
                             <li>School Rules & Regulations</li>
                         </ul>
                     </li>
-                    <li className={drop == 1 ? 'Navbar_LI_main' : "Navbar_LI_main Navbar_UL_SUB2"}  onMouseLeave={() => setDrop(0)}>Admission</li>
-                    <li className={drop == 1 ? 'Navbar_LI_main' : "Navbar_LI_main Navbar_UL_SUB2"}  onMouseLeave={() => setDrop(0)}>Co-Curriculum</li>
-                    <li className={drop == 1 ? 'Navbar_LI_main' : "Navbar_LI_main Navbar_UL_SUB2"}  onMouseLeave={() => setDrop(0)}>Facilities</li>
-                    <li className={drop == 1 ? 'Navbar_LI_main' : "Navbar_LI_main Navbar_UL_SUB2"}  onMouseLeave={() => setDrop(0)}>Growth Activities</li>
-                    <li className={drop == 1 ? 'Navbar_LI_main' : "Navbar_LI_main Navbar_UL_SUB2"}  onMouseLeave={() => setDrop(0)}>Support & Libraries</li>
-                    <li className={drop == 1 ? 'Navbar_LI_main' : "Navbar_LI_main Navbar_UL_SUB2"}  onMouseLeave={() => setDrop(0)}>Media Gallery</li>
+                    <li className={drop == 4 ? 'Navbar_LI_main' : "Navbar_LI_main Navbar_UL_SUB2"}  onMouseLeave={() => setDrop(0)}>
+                        <span onClick={() => setDrop(4)}>Admission</span>
+                        <ul className='Navbar_UL_SUB'>
+                            <li>Procedures</li>
+                            <li>Schedule</li>
+                            <li>Student Details</li>
+                        </ul>
+                    </li>
+                    <li className={drop == 5 || drop == 5.1 ? 'Navbar_LI_main' : "Navbar_LI_main Navbar_UL_SUB2"}  onMouseLeave={() => setDrop(0)}>
+                        <span  onClick={() => setDrop(5)}>Co-Curriculum</span>
+                        <ul className='Navbar_UL_SUB'>
+                            <li onClick={() => setDrop(5.1)}>
+                                <span>Clubs</span>
+                                <ul className={drop == 5.1 && 'Navbar_UL_mini'}  onMouseLeave={() => setDrop(5)}>
+                                    <li>Club Name</li>
+                                </ul>
+                            </li>
+                            <li>Project</li>
+                            <li>Events</li>
+                            <li>Excursion</li>
+                        </ul>
+                    </li>
+                    <li className={drop == 6 ? 'Navbar_LI_main' : "Navbar_LI_main Navbar_UL_SUB2"}  onMouseLeave={() => setDrop(0)}>
+                        <span onClick={() => setDrop(6)}>Facilities</span>
+                        <ul className='Navbar_UL_SUB'>
+                            <li>Laboratories</li>
+                            <li>Infirmary</li>
+                            <li>Transport</li>
+                            <li>Special Education</li>
+                            <li>Digital Classroom</li>
+                            <li>Class Work</li>
+                        </ul>
+                    </li>
+                    <li className={drop == 7 ? 'Navbar_LI_main' : "Navbar_LI_main Navbar_UL_SUB2"}  onMouseLeave={() => setDrop(0)}>
+                        <span onClick={() => setDrop(7)}>Growth Activities</span>
+                        <ul className='Navbar_UL_SUB'>
+                            <li>Accolades</li>
+                            <li>Workshops</li>
+                            <li>Teaching Methods</li>
+                            <li>Career Counselling</li>
+                            <li>School Intiatives</li>
+                        </ul>
+                    </li>
+                    <li className={drop == 8 ? 'Navbar_LI_main' : "Navbar_LI_main Navbar_UL_SUB2"}  onMouseLeave={() => setDrop(0)}>
+                        <span onClick={() => setDrop(8)}>Support & Libraries</span>
+                        <ul className='Navbar_UL_SUB'>
+                            <li>Mission</li>
+                            <li>Art Workshops</li>
+                            <li>Activities</li>
+                            <li>Know More</li>
+                        </ul>
+                    </li>
+                    <li className={drop == 9 ? 'Navbar_LI_main' : "Navbar_LI_main Navbar_UL_SUB2"}  onMouseLeave={() => setDrop(0)}>
+                        <span onClick={() => setDrop(9)}>Media Gallery</span>
+                        <ul className='Navbar_UL_SUB'>
+                            <li>Photo Gallery</li>
+                            <li>Video Gallery</li>
+                        </ul>
+                    </li>
                     <li className='Navbar_LI_main'>Contact Us</li>
                 </ul>
             </div>
